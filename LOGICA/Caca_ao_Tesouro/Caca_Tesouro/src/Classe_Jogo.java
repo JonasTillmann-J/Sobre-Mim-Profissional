82c2e46af5da6ceaf8fc54db00203bd9f35b0831
Tillmann.J <139597953+JonasTillmann-J@users.noreply.github.com>
2025-06-25 01:36:53 +0000

    a

--- LOGICA/Caca_ao_Tesouro/Caca_Tesouro/src/Classe_Jogo.java
+++ LOGICA/Caca_ao_Tesouro/Caca_Tesouro/src/Classe_Jogo.java
@@ -1,9 +1,45 @@
 public class Classe_Jogo {
+    
     public void painelGameInit(){
+        System.out.println(
+        "__________________________________\n"+
+        "|Jogo Caça ao Tesouro de Terminal|\n"+
+        "|________________________________|\n"+
+        "\nExplore as coordenadas de uma Ilha Misteriosa para\n"+
+        "tentar achar o Tesouro,o jogo acontece em um mapa\n"+
+        "de 8x8 tendo 5 armadilhas em pontos aleatorios do mapa\n"
+        );
+
         System.out.println(
         "______________________|\n"+
         "|Aperte S para começar|\n"+
         "|_____________________|\n"
         );
-    } 
+    }
+    public void MapGame(){
+        // Declaração da Matriz do Jogo
+        char[][] matriz = new char[9][9];
+        // Inicialização da Matriz com o caracter "~" que representa a areia
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 1; j < matriz.length; j++) {
+                matriz[i][j] = '~';
+            }
+        }
+
+        int contadorV = 0;
+
+        System.out.println("\n    1  2  3  4  5  6  7  8");
+        for (int i = 1; i < matriz.length; i++) {
+            System.out.println("");
+            for (int j = 1; j < matriz.length; j++) {
+                contadorV++;
+                    if (contadorV == 1) {
+                    System.out.print(""+(i)+"|  "+matriz[i][j]+"  ");
+                } else {
+                    System.out.print(matriz[i][j]+"  ");
+                }
+            }
+             contadorV = 0;
+        }
+    }
 }
--- LOGICA/Caca_ao_Tesouro/Caca_Tesouro/src/Main.java
+++ LOGICA/Caca_ao_Tesouro/Caca_Tesouro/src/Main.java
@@ -1,58 +1,13 @@
-import java.net.ContentHandler;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         //Declarações Globais
         Scanner sc = new Scanner(System.in);
         Classe_Jogo cj = new Classe_Jogo();
-        boolean runningGameStatus;
-        char[][] matriz = new char[9][9];
-
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz.length; j++) {
-                matriz[i][j] = '~';
-            }
-        }
-        System.out.println("\n");
-        int contadorV = 0;
-        int numeradorH = 0;
-        for (int i = 0; i < matriz.length; i++) {
-            System.out.println("");
-            for (int j = 0; j < matriz.length; j++) {
-                contadorV++;
-                if(j == 0){
-                    numeradorH ++;
-                    //System.out.print(""+numeradorH);
-                    System.out.print(/*i+"-"+j+"  "*/numeradorH);
-                } else if (contadorV == 1) {
-                    System.out.print("\n"+(i+1)+"|  "+i+"-"+j+"  ");
-                }else{
-                    System.out.print(+i+"-"+j+"  ");    
-                }
-                contadorV = 0;
-            }
-        }
-        /* 
-        for (int i = 0; i < matriz.length; i++) {
-            
-            System.out.println("");
-            for (int j = 0; j < matriz.length; j++) {
-                contadorV++;
-                if (contadorV == 1) {
-                    System.out.print((i+1)+"|  "+matriz[i][j]+"  ");
-                }else{
-                    System.out.print(matriz[i][j]+"  ");
-                }
-            }
-            contadorV = 0;
-        }
-        //Inicialização do Game
-        System.out.println("\n__________________________________\n|Jogo Caça ao Tesouro de Terminal|\n|________________________________|\n\nExplore as coordenadas de uma Ilha Misteriosa para tentar achar o Tesouro,\no jogo acontece em um mapa de 8x8 tendo 5 armadilhas em pontos aleatorios do mapa");
-        do {
-
-            char initGameResponse = sc.nextLine().charAt(0);
-            runningGameStatus = initGameResponse == 's' || initGameResponse == 'S' ? true : false;
-        } while (runningGameStatus);*/
+        //boolean runningGameStatus;
+        cj.painelGameInit();
+        cj.MapGame();
+              
         sc.close();
     }
 }
